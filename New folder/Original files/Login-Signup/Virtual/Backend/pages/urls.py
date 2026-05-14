56a1559637a4c20ad99815280113fd6dc5383fe9
Ahmed Sameh <a10101010h@gmail.com>
2026-05-14 16:02:05 +0300

    a

--- New folder/Original files/Login-Signup/Virtual/Backend/pages/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login', views.login, name='login'),
-    path('SignUp', views.signup, name='SignUp'),
-    path('index', views.index, name='index'),
-]
